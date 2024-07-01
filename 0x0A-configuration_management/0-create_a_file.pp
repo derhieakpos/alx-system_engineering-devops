f0c38e66277aaeeae93989b452426e088f5d713f
derhieakpos <derhieakpos@gmail.com>
2024-07-01 06:27:59 -0700

    Still not wprking well

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 
 file { '/tmp/school':
 ensure => file,
-content => 'I love puppet''
+content => 'I love puppet',
 mode => '0744',
 owner => 'www-data',
-group => 'www.data',
+group => 'www-data',
 }
